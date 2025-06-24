20ccc2e706a32bad8ea06cdb11503157c63eb649
Amrit Sundarka <amritsundarka@gmail.com>
2025-06-24 10:55:40 +0530

    cors addition

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import awsServerlessExpress from 'aws-serverless-express';
 
 const app = express();
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello from Express + TypeScript + Lambda' });
@@ -9,12 +11,10 @@ app.get('/', (_req, res) => {
 
 const server = awsServerlessExpress.createServer(app);
 
-// AWS Lambda export
 export const handler = (event: any, context: any) => {
   awsServerlessExpress.proxy(server, event, context);
 };
 
-// Local dev
 if (process.env.NODE_ENV !== 'production' && !process.env.AWS_LAMBDA_FUNCTION_NAME) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
